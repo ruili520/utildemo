d2ec5e4cd7403375c308c56afc13b9fd4946e258
LoveRuili <wb1028@163.com>
2019-01-15 17:36:48 +0800

    身份证验证工具类提交

---
+++ src/main/java/com/loveayc/utildemo/UtildemoApplication.java
@@ -0,0 +1,14 @@
+package com.loveayc.utildemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtildemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtildemoApplication.class, args);
+    }
+
+}
+
